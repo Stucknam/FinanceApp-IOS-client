3597c5791b6ca09592452051f0992f5fe37f14da
Stucknam <ad.gm1526@gmail.com>
2026-05-27 14:02:17 +0200

    Initial Commit

---
+++ FinanceApp/FinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppApp.swift
+//  FinanceApp
+//
+//  Created by user271129 on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
